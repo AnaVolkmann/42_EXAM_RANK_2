79510970ecc9e4125d5e527e8da601515204a270
Ana Laura Da Silva Volkmann <ana-lda-@c2r1s4.42porto.com>
2024-10-07 12:17:44 +0100

    ajust

--- level 4/sort_list/sort_list.c
+++ level 4/sort_list/sort_list.c
@@ -6,30 +6,30 @@
 /*   By: ana-lda- <ana-lda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:41:07 by ana-lda-          #+#    #+#             */
-/*   Updated: 2024/07/02 20:22:46 by ana-lda-         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:17:26 by ana-lda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "sort_list.h"
+#include <stdlib.h>
+#include "list.h"
 
-t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
-{
+t_list *sort_list(t_list* lst, int (*cmp)(int, int)) {
     int swap;
-    t_list *temp;
+    t_list *start;
 
-    temp = lst;
-    while(lst->next)
+    start = lst;
+
+    while (lst != NULL && lst->next != NULL)
     {
-        if (((*cmp)(lst->data, lst->next->data)) == 0)
-        {
+        if ((*cmp)(lst->data, lst->next->data) == 0)
+	    {
             swap = lst->data;
             lst->data = lst->next->data;
             lst->next->data = swap;
-            lst = temp;
+            lst = start;
         }
-        else
-            lst = lst->next;
+	    else
+		    lst = lst->next;
     }
-    lst = temp;
-    return (lst);
+    return (start);
 }
